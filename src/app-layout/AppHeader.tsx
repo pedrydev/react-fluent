c8b43fa811ea457ba8fce5ae03b902538abba0e0
Pedro Velazquez <pedrydev@outlook.com>
2023-09-08 22:14:04 -0400

    remove horizontal offset

--- src/app-layout/AppHeader.tsx
+++ src/app-layout/AppHeader.tsx
@@ -9,22 +9,22 @@ import {
   MenuTrigger,
   mergeClasses,
   Subtitle2,
-  tokens
-} from "@fluentui/react-components";
-import { SignOut24Regular } from "@fluentui/react-icons";
-import { useAuth } from "@/core/auth/contexts/AuthProvider.tsx";
-import AppIcon from "./AppIcon.tsx";
-import NotificationIcon from "./notifications/NotificationIcon.tsx";
-import HelpIcon from "@/app-layout/HelpIcon.tsx";
-import LanguageSwitch from "@/app-layout/LanguageSwitch.tsx";
+  tokens,
+} from '@fluentui/react-components';
+import { SignOut24Regular } from '@fluentui/react-icons';
+import { useAuth } from '@/core/auth/contexts/AuthProvider.tsx';
+import AppIcon from './AppIcon.tsx';
+import NotificationIcon from './notifications/NotificationIcon.tsx';
+import HelpIcon from '@/app-layout/HelpIcon.tsx';
+import LanguageSwitch from '@/app-layout/LanguageSwitch.tsx';
 
 const useStyles = makeStyles({
   header: {
-    backgroundColor: tokens.colorBrandBackground
+    backgroundColor: tokens.colorBrandBackground,
   },
   text: {
-    color: tokens.colorNeutralBackground1
-  }
+    color: tokens.colorNeutralBackground1,
+  },
 });
 
 export default function AppHeader() {
@@ -32,24 +32,24 @@ export default function AppHeader() {
   const { logout, user } = useAuth();
 
   return (
-    <header className={mergeClasses("h-12 flex items-center fixed top-0 left-0 right-0", styles.header)}>
+    <header className={mergeClasses('h-12 flex items-center fixed top-0 left-0 right-0', styles.header)}>
       <AppIcon />
-      <div className="ml-1.5 flex flex-1 items-center justify-between h-full">
+      <div className='ml-1.5 flex flex-1 items-center justify-between h-full'>
         <Subtitle2 className={styles.text}>{import.meta.env.VITE_APP_NAME}</Subtitle2>
-        <nav className="flex space-x-2">
+        <nav className='flex space-x-2'>
           <NotificationIcon />
           <HelpIcon />
           <LanguageSwitch />
         </nav>
       </div>
-      <div className="h-12 w-12 flex items-center justify-center">
-        <Menu positioning={{ position: "below", align: "end", offset: { mainAxis: 6, crossAxis: 12 } }}>
+      <div className='h-12 w-12 flex items-center justify-center'>
+        <Menu positioning={{ position: 'below', align: 'end', offset: { mainAxis: 6 } }}>
           <MenuTrigger disableButtonEnhancement>
-            <Avatar className="hover:cursor-pointer" name={`${user.username} image profile}`}
+            <Avatar className='hover:cursor-pointer' name={`${user.username} image profile}`}
                     image={{ src: user.profileUrl }} />
           </MenuTrigger>
           <MenuPopover>
-            <div className="flex justify-center">
+            <div className='flex justify-center'>
               <Subtitle2>{user.username}</Subtitle2>
             </div>
             <MenuDivider />
