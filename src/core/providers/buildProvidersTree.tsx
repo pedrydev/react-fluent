312c0d15f76ea734def07ddf783678b0e08fcb40
Pedro Velazquez <pedrydev@outlook.com>
2024-01-09 18:42:31 -0500

    refactor code style

--- src/core/providers/buildProvidersTree.tsx
+++ src/core/providers/buildProvidersTree.tsx
@@ -1,6 +1,8 @@
 import { JSXElementConstructor, PropsWithChildren } from 'react';
 
-export type BuildProvidersTreeArgs = [JSXElementConstructor<any>] | [JSXElementConstructor<any>, { [key: string]: any }]
+export type BuildProvidersTreeArgs =
+  [JSXElementConstructor<any>] |
+  [JSXElementConstructor<any>, { [key: string]: any }]
 
 // https://x.com/_georgemoller/status/1744331324037419145?s=20
 export default function buildProvidersTree(providers: BuildProvidersTreeArgs[]) {
